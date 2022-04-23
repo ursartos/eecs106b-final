d5ae25dce5b2a8332177d05264ccf3922c500ea9
Tarun Amarnath <tarunamarnath@berkeley.edu>
2022-04-22 19:21:34 -0700

    Add contrast to feature

--- scripts/feature_extractor.py
+++ scripts/feature_extractor.py
@@ -1,14 +1,23 @@
 import cv2
 import numpy as np
 
+
 def extract_features(img_path):
     img = cv2.imread(img_path)
-    
+
+    # edge = cv2.Canny(img, 200, 250, L2gradient=True)
+    # cv2.imshow('image', img)
+    # cv2.imshow('edge', edge)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
     features = []
     features.extend(average_hsvs(img))
     features.extend(average_rgbs(img))
-    features.extend(gabor_filters(img))
+    # features.extend(gabor_filters(img))
+    features.append(rms_contrast(img))
     return features
+
 
 def gabor_filters(img):
     # Applies several Gabor filters with varied hyperparameters
@@ -17,15 +26,17 @@ def gabor_filters(img):
 
     for i in range(num_filters):
         # Gets kernel with specified parameters
-        kernel = cv2.getGaborKernel((15, 15), 3, np.pi * i / num_filters, 10, 0.5, 0)
+        kernel = cv2.getGaborKernel(
+            (15, 15), 3, np.pi * i / num_filters, 10, 0.5, 0)
         # Normalization
         kernel /= kernel.sum() * 1.0
-        # Apply filter        
+        # Apply filter
         filtered = cv2.filter2D(img, -1, kernel)
-        
+
         filtered_images.append(filtered)
 
     return filtered_images
+
 
 def average_hsvs(img):
     # Convert to hsv
@@ -34,8 +45,16 @@ def average_hsvs(img):
     averages = [np.mean(img[:, :, i]) for i in range(3)]
     return averages
 
+
 def average_rgbs(img):
     averages = [np.mean(img[:, :, i]) for i in range(3)]
     return averages
-    
-print(extract_features("images/bluffed-acclaiming.jpg"))
+
+# Gets root mean square contrast, which is essentially standard deviation of intensities
+def rms_contrast(img):
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img_grey.std()
+
+
+print(extract_features("images/sand.jpg"))
+print(extract_features("images/grass.jpg"))
