08f85634c509510b5eebc62d709adb05c6cf0f32
Kaushik Shivakumar <kaushikshivakumar@kaushiks-mbp-2.lan>
2022-04-24 21:59:37 -0700

    work on dijkstra planner

--- src/proj2_pkg/src/proj2/planners/optimization_planner_dijkstra.py
+++ src/proj2_pkg/src/proj2/planners/optimization_planner_dijkstra.py
@@ -1,211 +1,47 @@
-from casadi import Opti, sin, cos, tan, vertcat, mtimes, floor, conditional, MX, if_else, logic_and
+from casadi import Opti, sin, cos, tan, vertcat, mtimes, floor, conditional, MX, SX, if_else, logic_and
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-TERRAIN_DIM = 1
-def current_pos_to_terrain(self, pos, terrains):
-    terrain_vec = np.zeros(TERRAIN_DIM + 1)
-    for i, terrain in enumerate(terrains):
-        terrain_corners = terrain[0]
-        if terrain_corners[0] <= pos[0] <= terrain_corners[1] and terrain_corners[2] <= pos[1] <= terrain_corners[3]:
-            terrain_vec[i + 1] = 1
-            return terrain_vec, terrain[1], terrain[2]
-    terrain_vec[0] = 1
-    return terrain_vec, 1, 1
+from dijkstar import Graph, find_path
 
-def unicycle_robot_model(q, u, dt=0.01, terrains=[]):
-    """
-    Implements the discrete time dynamics of your robot.
-    i.e. this function implements F in
-
-    q_{t+1} = F(q_{t}, u_{t})
-
-    dt is the discretization timestep.
-    L is the axel-to-axel length of the car.
-
-    q = array of casadi MX.sym symbolic variables [x, y, theta, phi].
-    u = array of casadi MX.sym symbolic variables [u1, u2] (velocity and steering inputs).
-
-    Use the casadi sin, cos, tan functions.
-
-    The casadi vertcat or vcat functions may also be useful. Note that to turn a list of 
-    casadi expressions into a column vector of those expressions, you should use vertcat or
-    vcat. vertcat takes as input a variable number of arguments, and vcat takes as input a list.
-
-    Example:
-        x = MX.sym('x')
-        y = MX.sym('y')
-        z = MX.sym('z')
-
-        q = vertcat(x + y, y, z) # makes a 3x1 matrix with entries x + y, y, and z.
-        # OR
-        q = vcat([x + y, y, z]) # makes a 3x1 matrix with entries x + y, y, and z.
-    """
-    x,y,theta,v,omega = q[0], q[1], q[2], u[0], u[1]
-    # d, k = terrains_d[0, 0], terrains_k[0, 0]
-    # d = conditional(floor(x), terrains_d, terrains_d[0, 0], False)
-    # k = conditional(floor(x), terrains_k, terrains_k[0, 0], False)
-
-    # fx = floor(x)
-    # fy = floor(y)
-    # d, k = 1, 1
-    # d = if_else(fx, terrains_d[fx, fy], terrains_d[0, 0])
-
-    d = MX(1)
-    k = MX(1)
-    for terrain in terrains:
-        x1, y1, x2, y2 = terrain[0]
-        dter, kter = terrain[1], terrain[2]
-        d = if_else(logic_and(y < y2, logic_and(x < x2, logic_and(x1 < x, y1 < y))), MX(dter), d)
-        k = if_else(logic_and(y < y2, logic_and(x < x2, logic_and(x1 < x, y1 < y))), MX(kter), k)
-
-    q_dot = vertcat(d*v*cos(theta), d*v*sin(theta), k*omega)
-
-    return q + dt*q_dot
-
-def initial_cond(q_start, q_goal, n):
-    """
-    Construct an initial guess for a solution to "warm start" the optimization.
-
-    An easy way to initialize our optimization is to say that our robot will move 
-    in a straight line in configuration space. Of course, this isn't always possible 
-    since our dynamics are nonholonomic, but we don't necessarily need our initial 
-    condition to be feasible. We just want it to be closer to the final trajectory 
-    than any of the other simple initialization strategies (random, all zero, etc).
-
-    We'll set our initial guess for the inputs to zeros to hopefully bias our solver into
-    picking low control inputs
-
-    n is the number of timesteps.
-
-    This function will return two arrays: 
-        q0 is an array of shape (4, n+1) representing the initial guess for the state
-        optimization variables.
-
-        u0 is an array of shape (2, n) representing the initial guess for the state
-        optimization variables.
+def position_to_grid(terrains, pos, side_length):
+    return (pos/side_length * len(terrains)).astype(int)
     
-    """
-    q0 = np.zeros((3, n + 1))
-    u0 = np.zeros((2, n))
+def xy_to_i(terrain_map, xy):
+    return len(terrain_map) * xy[0] + xy[1]
 
-    # Your code here.
-    xs = np.linspace(q_start[0],q_goal[0],n+1).reshape((1,n+1))
-    ys = np.linspace(q_start[1],q_goal[1],n+1).reshape((1,n+1))
-    thetas = np.linspace(q_start[2],q_goal[2],n+1).reshape((1,n+1))
+def i_to_xy(terrain_map, i):
+    return np.array([i % len(terrain_map), i // len(terrain_map)])
 
-    q0 = vertcat(xs,ys,thetas)
+def shortest_path_to_goal(terrains_grid, side_length, start, goal):
+    graph = Graph()
+    start_node = xy_to_i(terrains_grid, position_to_grid(terrains_grid, start, side_length))
+    goal_node = xy_to_i(terrains_grid, position_to_grid(terrains_grid, goal, side_length))
 
-    # q0[:2,:] = np.reshape(np.linspace(q_start[:2], q_goal[:2], n+1), (1001,1))
+    for i in range(len(terrains_grid)):
+        for j in range(len(terrains_grid[0])):
+            current_node_idx = xy_to_i(terrains_grid, [i, j])
+            
+            for r in range(-1, 2):
+                for c in range(-1, 2):
+                    if r == 0 and c == 0:
+                        continue
+                    if i + r < 0 or i + r >= len(terrains_grid):
+                        continue
+                    if j + c < 0 or j + c >= len(terrains_grid[0]):
+                        continue
+                    neighbor_node_idx = xy_to_i(terrains_grid, [i + r, j + c])
+                    min_r, min_c = min(i, i + r), min(j, j + c)
+                    graph.add_edge(current_node_idx, neighbor_node_idx, 1/terrains_grid[min_r, min_c, 0])
 
-    return q0, u0
+    pathinfo = find_path(graph, start_node, goal_node)
 
-def objective_func(q, u, q_goal, Q, R, P):
-    """
-    Implements the objective function. q is an array of states and u is an array of inputs. Together,
-    these two arrays contain all the optimization variables in our problem.
+    real_world_points = []
+    for i in range(len(pathinfo)):
+        real_world_points.append(i_to_xy(terrains_grid, pathinfo[i]) * side_length / len(terrains_grid))
 
-    In particular, 
-
-    q has shape (3, N+1), so that each column of q is an array q[:, i] = [q0, q1, q2, q3]
-    (i.e. [x, y, theta, phi]), the state at time-step i. 
-
-    u has shape (2, N), so that each column of u is an array u[:, i] = [u1, u2], the two control inputs 
-    (velocity and steering) of the unicycle model.
-
-    This function should create an expression of the form
-
-    sum_{i = 1, ..., N} ((q(i) - q_goal)^T * Q * (q(i) - q_goal) + (u(i)^T * R * u(i)))
-    + (q(N+1) - q_goal)^T * P * (q(N+1) - q_goal)
-
-    Note: When dealing with casadi symbolic variables, you can use @ for matrix multiplication,
-    and * for standard, numpy-style element-wise (or broadcasted) multiplication.
-    
-    """
-
-    n = q.shape[1] - 1
-    obj = 0
-    for i in range(n):
-        qi = q[:, i]
-        ui = u[:, i]
-
-        # Define one term of the summation here: ((q(i) - q_goal)^T * Q * (q(i) - q_goal) + (u(i)^T * R * u(i)))
-        term = mtimes(mtimes((qi - q_goal).T,Q),(qi - q_goal)) + mtimes(mtimes(ui.T,R),ui)
-        # term = np.matmul(np.matmul(np.transpose(qi - q_goal), Q), qi - q_goal) + np.matmul(np.matmul(np.transpose(ui), R), ui)
-        obj += term
-
-    q_last = q[:, n]
-    # Define the last term here: (q(N+1) - q_goal)^T * P * (q(N+1) - q_goal)
-    term_last = mtimes(mtimes((q_last - q_goal).T,P),(q_last - q_goal))
-    # term_last = np.matmul(np.matmul(np.transpose(q_last - q_goal), P), q_last - q_goal)
-    obj += term_last
-    return obj
-
-def constraints(q, u, q_lb, q_ub, u_lb, u_ub, obs_list, q_start, q_goal, L=0.3, dt=0.01, terrains=[]):
-    """
-    Constructs a list where each entry is a casadi.MX symbolic expression representing
-    a constraint of our optimization problem.
-
-    q has shape (3, N+1), so that each column of q is an array q[:, i] = [q0, q1, q2, q3]
-    (i.e. [x, y, theta, phi]), the state at time-step i. 
-
-    u has shape (2, N), so that each column of u is an array u[:, i] = [u1, u2], the two control inputs 
-    (velocity and steering) of the unicycle model.
-
-    q_lb is a size (3,) array [x_lb, y_lb, theta_lb, phi_lb] containing lower bounds for each state variable.
-
-    q_ub is a size (3,) array [x_ub, y_ub, theta_ub, phi_ub] containing upper bounds for each state variable.
-
-    u_lb is a size (2,) array [u1_lb, u2_lb] containing lower bounds for each input.
-
-    u_ub is a size (2,) array [u1_ub, u2_ub] containing upper bounds for each input.
-
-    obs_list is a list of obstacles, where each obstacle is represented by  3-tuple (x, y, r)
-            representing the (x, y) center of the obstacle and its radius r. All obstacles are modelled as
-            circles.
-
-    q_start is a size (3,) array representing the starting state of the plan.
-
-    q_goal is a size (3,) array representing the goal state of the plan.
-
-    L is the axel-to-axel length of the car.
-
-    dt is the discretization timestep.
-
-    """
-    constraints = []
-
-    # State constraints
-    constraints.extend([q_lb[0] <= q[0, :], q[0, :] <= q_ub[0]])
-    constraints.extend([q_lb[1] <= q[1, :], q[1, :] <= q_ub[1]])
-    
-    # Input constraints
-    constraints.extend([u_lb[0] <= u[0, :], u[0, :] <= u_ub[0]])
-    constraints.extend([u_lb[1] <= u[1, :], u[1, :] <= u_ub[1]])
-
-    # Dynamics constraints
-    for t in range(q.shape[1] - 1):
-        q_t   = q[:, t]
-        q_tp1 = q[:, t + 1]
-        u_t   = u[:, t]
-        constraints.append(q_tp1 == unicycle_robot_model(q_t, u_t, terrains=terrains)) # You should use the unicycle_robot_model function here somehow.
-
-    # Obstacle constraints
-    for obj in obs_list:
-        obj_x, obj_y, obj_r = obj
-        for t in range(q.shape[1]):
-            constraints.append((q[0,t]-obj_x)**2 + (q[1,t]-obj_y)**2 >= obj_r**2) # Define the obstacle constraints.
-
-    # Initial and final state constraints
-    constraints.append(q_start == q[:,0]) # Constraint on start state.
-    constraints.append(q_goal == q[:,q.shape[1]-1]) # Constraint on final state.
-
-    # for c in constraints:
-    #     print(c, c.is_constant())
-    return constraints
-
-def plan_to_pose(q_start, q_goal, q_lb, q_ub, u_lb, u_ub, obs_list, n=1000, dt=0.01, terrains=[]):
+def plan_to_pose(q_start, q_goal, q_lb, q_ub, u_lb, u_ub, obs_list, n=1000, dt=0.01, terrain_map=None, side_length=None):
     """
     Plans a path from q_start to q_goal.
 
@@ -219,48 +55,12 @@ def plan_to_pose(q_start, q_goal, q_lb, q_ub, u_lb, u_ub, obs_list, n=1000, dt=0
     Returns a plan (shape (3, n+1)) of waypoints and a sequence of inputs
     (shape (2, n)) of inputs at each timestep.
     """
-    opti = Opti()
+    
 
-    q = opti.variable(3, n + 1)
-    u = opti.variable(2, n)
+def path_to_trajectory(path, terrain_map, side_length):
+    res = terrain_map.shape[0]/side_length
 
-    Q = np.diag([1, 1, 1])
-    R = 2 * np.diag([1, 0.5])
-    P = n * Q
-
-    # terrain_kds = np.ones((q_ub[0] - q_lb[0], q_ub[1] - q_lb[1], 2))
-    # if terrains is not None:
-    #     for terrain in terrains:
-    #         xmin, xmax, ymin, ymax = terrain[0]
-    #         k, d = terrain[1:]
-            # terrain_kds[xmin:xmax, ymin:ymax, :] = [k, d]
-
-    q0, u0 = initial_cond(q_start, q_goal, n)
-    obj = objective_func(q, u, q_goal, Q, R, P)
-
-    # terrains = MX(terrain_kds[:, :, 1])
-
-    opti.minimize(obj)
-
-    opti.subject_to(constraints(q, u, q_lb, q_ub, u_lb, u_ub, obs_list, q_start, q_goal, dt=dt, terrains=terrains))
-
-    opti.set_initial(q, q0)
-    opti.set_initial(u, u0)
-
-    ###### CONSTRUCT SOLVER AND SOLVE ######
-
-    opti.solver('ipopt')
-    p_opts = {"expand": False}
-    s_opts = {"max_iter": 1e4}
-
-    opti.solver('ipopt', p_opts, s_opts)
-    sol = opti.solve()
-
-    plan = sol.value(q)
-    inputs = sol.value(u)
-    return plan, inputs
-
-def plot(plan, inputs, times, q_lb, q_ub, obs_list):
+def plot(plan, inputs, times, q_lb, q_ub, obs_list, terrains):
 
     # Trajectory plot
     ax = plt.subplot(1, 1, 1)
@@ -272,6 +72,15 @@ def plot(plan, inputs, times, q_lb, q_ub, obs_list):
         xc, yc, r = obs
         circle = plt.Circle((xc, yc), r, color='black')
         ax.add_artist(circle)
+    
+    for ter in terrains:
+        # print(ter)
+        w, h = ter[0][1] - ter[0][0], ter[0][3] - ter[0][2]
+        x, y = ter[0][0], ter[0][2]
+        # print(x,y,w,h)
+        # x, y = 4,4
+        rect = plt.Rectangle((x, y), w, h, color='grey')
+        ax.add_artist(rect)
 
     plan_x = plan[0, :]
     plan_y = plan[1, :]
@@ -305,15 +114,15 @@ def main():
     ###### PROBLEM PARAMS ######
 
     n = 1000
-    dt = 0.01
+    dt = 0.5
 
-    xy_low = [-5, -1]
-    xy_high = [10, 10]
+    xy_low = [0, 0]
+    xy_high = [4, 4]
     u1_max = 2
     u2_max = 3
-    obs_list = [[5, 5, 1], [-3, 4, 1], [4, 2, 2]]
-    q_start = np.array([-5, -1, 0])
-    q_goal = np.array([-5, 2, np.pi/2])
+    obs_list = [[2, 1, 1]]#, [-3, 4, 1], [4, 2, 2]]
+    q_start = np.array([0, 0, 0])
+    q_goal = np.array([4, 0, 0])
 
     ###### SETUP PROBLEM ######
     
@@ -325,15 +134,24 @@ def main():
 
     ###### CONSTRUCT SOLVER AND SOLVE ######
 
-    terrain1 = [([0, 3, 0, 3], 0.4, 0.4)]
+    terrain1 = ([1, 2, 0, 1], 0.05, 0.05)
+    terrains = [terrain1]
 
-    plan, inputs = plan_to_pose(q_start, q_goal, q_lb, q_ub, u_lb, u_ub, obs_list, n=n, dt=dt, terrains=terrain1)
+    res = 10
+    side_length = q_lb[0] - q_ub[0]
+    terrain_map = np.ones((res*side_length, res*side_length, 2))
+    for terrain in terrains:
+        xmin, xmax, ymin, ymax = res*terrain[0]
+        k, d = terrain[1:]
+        terrain_map[xmin:xmax, ymin:ymax, :] = [k, d]
+
+    plan, inputs = plan_to_pose(q_start, q_goal, q_lb, q_ub, u_lb, u_ub, obs_list, n=n, dt=dt, terrain_map=terrains, side_length=side_length)
 
     ###### PLOT ######
 
     times = np.arange(0.0, (n + 1) * dt, dt)
     print("Final Position:", plan[:3, -1])
-    plot(plan, inputs, times, q_lb, q_ub, obs_list)
+    plot(plan, inputs, times, q_lb, q_ub, obs_list, terrains)
 
 if __name__ == '__main__':
     main()
