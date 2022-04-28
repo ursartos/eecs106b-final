6c4c72f67a8fd0fbd77fc5f5837eeaf1ad69d8c6
Tarun Amarnath <tarunamarnath@berkeley.edu>
2022-04-28 16:05:45 -0700

    Modified structure of takeImage

--- src/proj2_pkg/src/proj2/vision/takeImage.py
+++ src/proj2_pkg/src/proj2/vision/takeImage.py
@@ -1,49 +1,32 @@
 #! /usr/bin/env python
 # rospy for the subscriber
 import cv2
-from numpy.lib.npyio import save
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import numpy as np
-
-# Instantiate CvBridge
-bridge = CvBridge()
-save_image = True
-subscriber = None
 
 
-def takeImage(data):
-    """
-    Goal: knowing that you are already in the correct position, take an image and return it
-    Baxter topic: /cameras/right_hand_camera/image
-    """
-    global save_image
-    if (save_image):
-        print("Received an image!")
-        try:
-            # Convert ROS Image message to CV2 and save
-            cv2_img = bridge.imgmsg_to_cv2(data, "bgr8")
-            cv2.imwrite('camera_image.png', cv2_img)
-            exit
+class Imager():
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.save = True
 
-        except CvBridgeError as e:
-            print(e)
+        # Node cycle rate (in Hz).
+        self.loop_rate = rospy.Rate(1)
 
-def startImages():
-    global subscriber 
-    # Image topic
-    image_topic = '/camera/rgb/image_color'
-    # Subscriber setup
-    subscriber = rospy.Subscriber(image_topic, Image, takeImage)
+        # Subscribers
+        rospy.Subscriber('/camera/rgb/image_color', Image, self.takeImage)
 
-def stopImages():
-    global save_image
-    global subscriber
-
-    save_image = False
-    subscriber.unregister()
+    def takeImage(self, data):
+        """
+        Goal: knowing that you are already in the correct position, take an image and return it
+        Baxter topic: /cameras/right_hand_camera/image
+        """
+        # Convert ROS Image message to CV2 and save
+        cv2_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        cv2.imwrite('camera_image.png', cv2_img)
 
 
 if __name__ == '__main__':
-    startImages()
+    rospy.init_node('Imager')
+    node = Imager()
