4ed8eaaf8dfbe8a1186c2300d850158b26372b6d
Class Account <ee106b-abd@c105-1.local>
2022-04-29 10:24:37 -0700

    Fix bugs in homography pipeline

--- src/proj2_pkg/src/proj2/vision/analyze.py
+++ src/proj2_pkg/src/proj2/vision/analyze.py
@@ -3,21 +3,21 @@ import numpy as np
 
 # A list of points corresponding to the corners of the floor in the robot image,
 # specified in the base link frame's coordinate system
-POINTS = np.array([[ 2.5,    1.562,  0.   ]
-                   [ 2.5,   -1.592,  0.   ]
-                   [ 0.54,  -0.395,  0.   ]
+POINTS = np.array([[ 2.5,    1.562,  0.   ],
+                   [ 2.5,   -1.592,  0.   ],
+                   [ 0.54,  -0.395,  0.   ],
                    [ 0.54,   0.369,  0.   ]])
-VOXEL_SIZE = 0.5
+VOXEL_SIZE = 0.2
 PIXELS_PER_VOXEL = 50
 
-GRID_X_MIN = -10
-GRID_Y_MIN = -10
-GRID_X_MAX = 10
-GRID_Y_MAX = 10
+GRID_X_MIN = -5
+GRID_Y_MIN = -5
+GRID_X_MAX = 5
+GRID_Y_MAX = 5
 
 g_w = int(round((GRID_X_MAX - GRID_X_MIN)/VOXEL_SIZE))
 g_h = int(round((GRID_Y_MAX - GRID_Y_MIN)/VOXEL_SIZE))
-grid = np.zeros((g_h, g_w, 3))
+grid = np.zeros((g_h, g_w, 3), dtype=float)
 
 PPV = PIXELS_PER_VOXEL
 
@@ -40,7 +40,7 @@ def dostuff(image, cam_matrix, T_world_base, T_rgb_world):
 
     # Project the bounds of the camera into the world frame
     # Then determine the range of voxels the camera image covers
-    points_t = rot_base_world @ points.T + trans_base_world.reshape((3,1))
+    points_t = np.matmul(rot_base_world, POINTS.T) + np.array(trans_base_world).reshape((3,1))
     x_min = np.floor(np.min(points_t[0]) / VOXEL_SIZE) * VOXEL_SIZE
     x_max = np.ceil(np.max(points_t[0]) / VOXEL_SIZE) * VOXEL_SIZE
     y_min = np.floor(np.min(points_t[1]) / VOXEL_SIZE) * VOXEL_SIZE
@@ -62,18 +62,16 @@ def dostuff(image, cam_matrix, T_world_base, T_rgb_world):
 
     # Project the corners of the voxel range into the camera image
     # The homography will be found between these corners and corners of the reprojected image
-    p_rgb = rot_world_rgb @ np.array(rectangle).T + trans_world_rgb.reshape((3,1))
-    rect_image = project_points_float(p_depth, cam_matrix).T
+    p_rgb = np.matmul(rot_world_rgb, np.array(rectangle).T) + np.array(trans_world_rgb).reshape((3,1))
+    rect_image = project_points_float(p_rgb, cam_matrix).T
     dest = np.array(dest).astype(np.float32)
 
-    mask_like_img = np.ones(images[2].shape[:2], dtype=np.uint8)
-
-    # Reproject both the original image and a mask
-    # The mask is used to determine what areas of the reprojected image are valid
+    # Reproject the original image. Use RGBA matrix for computing masks
     matrix = cv2.getPerspectiveTransform(rect_image, dest)
-    warped = cv2.warpPerspective(images[2], matrix, (W, H))
-    mask = cv2.warpPerspective(mask_like_img, matrix, (W, H))
-    warped_img = cv2.cvtColor(warped, cv2.COLOR_BGR2RGB) # Change color spaces
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+    warped = cv2.warpPerspective(image, matrix, (W, H), cv2.INTER_CUBIC)
+
+    cv2.imshow('image', warped)
 
     for x in range(0, voxels_x):
         for y in range(0, voxels_y):
@@ -81,18 +79,17 @@ def dostuff(image, cam_matrix, T_world_base, T_rgb_world):
             gx = x + int(round((x_min - GRID_X_MIN)/VOXEL_SIZE))
             gy = y + int(round((y_min - GRID_Y_MIN)/VOXEL_SIZE))
 
-            img = warped_img[ y*PPV:(y+1)*PPV, x*PPV:(x+1)*PPV ]
-            m = mask[ y*PPV:(y+1)*PPV, x*PPV:(x+1)*PPV ]
-            n = np.sum(m)
+            img = warped[ y*PPV:(y+1)*PPV, x*PPV:(x+1)*PPV ]
+            n = np.sum(img[:,:,3])
 
             # Only handle valid regions in the reprojected image
-            if n > 0:
+            if n > 0 and gx >= 0 and gy >= 0 and gx < g_w and gy < g_h:
                 # FEATURE CODE
                 # PUT BETTER STUFF HERE
-                average = img.sum(axis=(0,1)) / n
+                average = img.astype(float).sum(axis=(0,1))[:3] / n
                 grid[gy, gx] = average
 
-    cv2.imshow('grid', cv2.resize(grid, None, fx=10, fy=10))
+    cv2.imshow('grid', cv2.resize(grid, None, fx=20, fy=20, interpolation=cv2.INTER_NEAREST))
     cv2.waitKey(1)
 
 # Modified from 106a lab
